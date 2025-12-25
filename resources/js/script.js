a6f9186cac7c677b71e14ccfe3934fb0ae3dd2f6
Fiiel <neildavid618@gmail.com>
2025-12-25 21:31:24 +0800

    added responsiveness and javascript animations

---
+++ resources/js/script.js
@@ -0,0 +1,16 @@
+let navLinks = document.querySelectorAll('nav ul li a');
+
+navLinks.forEach((item) => {
+    item.addEventListener('click', function(){
+        document.querySelector('nav ul li a.active').classList.remove('active');
+        item.classList.add('active');
+
+        document.querySelector('main > section.active').classList.remove('active');
+        document.querySelector(`main > section${item.getAttribute('href')}`).classList.add('active');
+    })
+    
+})
+
+document.querySelector('#sidebar .toggle-sidebar').addEventListener('click', function(){
+    document.querySelector('#sidebar').classList.toggle('open')
+})
